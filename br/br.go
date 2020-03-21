692ba65eb6a31016e1e40a4b2090aec0f2b8893f
Dexter Haslem <dexter@haslemtech.com>
2020-03-20 23:49:22 -0600

    trying to flesh out start of models and sessions

--- br/br.go
+++ br/br.go
@@ -1,23 +1,102 @@
 package br
 
 import (
-	"github.com/google/uuid"
+	"log"
+	"net/http"
+
 	"github.com/gorilla/websocket"
 	"github.com/labstack/echo"
 )
 
-var c *websocket.Conn
-var clients = make(map[*websocket.Conn]bool) // connected clients
-//var broadcast = make(chan Message)           // broadcast channel
+// dont do anything clever to map sessions, they are looked up by all three ids
+var sessions []*gameSes
 var upgrader = websocket.Upgrader{}
+
+func setAdminWs(s *gameSes, ctx echo.Context) error {
+
+}
+
+func setBlueWs(s *gameSes, ctx echo.Context) error {
+
+}
+
+func setRedWs(s *gameSes, ctx echo.Context) error {
+
+}
+
+func addReadOnlyWs(s *gameSes, ctx echo.Context) error {
+
+}
+
+func upgradeWS(ctx echo.Context) *websocket.Conn {
+	ws, err := upgrader.Upgrade(ctx.Response(), ctx.Request(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return ws
+}
+
+func ses(ctx echo.Context, st sesType) (*gameSes, error) {
+	id := c.Param("id")
+
+	for _, s := range sessions {
+		var sid sesID
+		switch st {
+		case admin:
+			sid = s.sesID
+		case blue:
+			sid = s.blueSesID
+		case red:
+			sid = s.redSesID
+		}
+
+		if sid == id {
+			return s, nil
+		}
+	}
+	return nil, c.String(http.StatusBadRequest, "no session")
+}
+
+func adminHandler(c echo.Context) error {
+	return c.String(http.StatusOK, "admin")
+}
+
+func adminWsSesHandler(c echo.Context) error {
+	s, err := ses(c, admin)
+	if err != nil {
+		return err
+	}
+
+	s.adminWs = upgradeWS(c)
+	return ctx.NoContent(http.StatusSwitchingProtocols)
+}
+
+func blueWsSesHandler(c echo.Context) error {
+	s, err := ses(c, blue)
+	if err != nil {
+		return err
+	}
+
+	s.blueWs = upgradeWS(c)
+	return ctx.NoContent(http.StatusSwitchingProtocols)
+}
+
+func redWsSesHandler(c echo.Context) error {
+	s, err := ses(c, red)
+	if err != nil {
+		return err
+	}
+
+	s.redWs = upgradeWS(c)
+	return ctx.NoContent(http.StatusSwitchingProtocols)
+}
 
 // Start fires up the battlerite system. cfgDir sets where to look for config jsons, conn is the echo connection string, eg ":1234"
 func Start(cfgDir string, conn string) {
-	u, _ := uuid.NewRandom()
 	e := echo.New()
 	e.GET("/admin", adminHandler)
-	e.GET("/admin/:id", adminSesHandler)
-	e.GET("/blue/:id", blueSesHandler)
-	e.GET("/red/:id", redSesHandler)
+	e.GET("/ws/admin/:id", adminSesHandler)
+	e.GET("/ws/blue/:id", blueSesHandler)
+	e.GET("/ws/red/:id", redSesHandler)
 	e.Logger.Fatal(e.Start(conn))
 }
--- br/models.go
+++ br/models.go
@@ -1,14 +1,44 @@
 package br
 
-import "github.com/google/uuid"
+import (
+	"github.com/google/uuid"
+	"github.com/gorilla/websocket"
+)
 
 type sesID uuid.UUID
 
-type adminSes struct {
-	mapName      string
-	blueName     string
-	redName      string
+type sesType int
+
+const (
+	// admin session type
+	admin sesType = 1
+	// blue captain session type
+	blue sesType = 2
+	// red captain session type
+	red sesType = 3
+)
+
+type gameSes struct {
+	mapName  string
+	blueName string
+	redName  string
+
+	// admin session id
+	sesID        sesID
 	redSesID     sesID
 	blueSesID    sesID
 	resultsSesID sesID
+
+	adminWs *websocket.Conn
+	redWs   *websocket.Conn
+	blueWs  *websocket.Conn
+	roWs    []*websocket.Conn
+}
+
+func startSes(mapName, blueName, redName string) *gameSes {
+	ns := gameSes
+	ns.mapName = mapName
+	ns.blueName = blueName
+	ns.redName = redName
+	return &ns
 }
